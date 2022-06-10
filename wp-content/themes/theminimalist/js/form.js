39b51a4a23792d42b1459a1a6dea4d5b00059191
Pankajchalke <pankaj.chalke@theminimalist.in>
2022-06-10 14:51:51 +0530

    checkbox updated

--- wp-content/themes/theminimalist/js/form.js
+++ wp-content/themes/theminimalist/js/form.js
@@ -39,6 +39,54 @@ $(document).ready(function() {
   })
 
 
+$(document).ready(function(){
+    $('a[data-toggle="tab"]').on('show.bs.tab', function(e) { 
+        localStorage.setItem('activeTab', $(e.target).attr('href'));
+    });
+    var activeTab = localStorage.getItem('activeTab'); 
+    if(activeTab){
+        $('#myTab a[href="' + activeTab + '"]').tab('show');
+        $('#myTab a[href="' + activeTab + '"]').addClass('active');
+    }
+});
+
+$(document).ready(function() {
+        $('.field').on('input', function() {
+            $(this).parent().parent().toggleClass('not-empty', this.value.trim().length > 0);
+        });
+        $(function() {
+            $('.field').focusout(function() {
+                var text_val = $(this).val();
+                $(this).parent().parent().toggleClass('not-empty', text_val !== "");
+            }).focusout();
+        });
+        $('.ctcfield').on('input', function() {
+            $(this).parent().parent().parent().toggleClass('not-empty', this.value.trim().length > 0);
+        });
+        $(function() {
+            $('.ctcfield').focusout(function() {
+                var text_val = $(this).val();
+                $(this).parent().parent().parent().toggleClass('not-empty', text_val !== "");
+            }).focusout();
+        });
+        $('.uploadfield').on('input', function() {
+            $(this).parent().parent().parent().parent().toggleClass('not-empty', this.value.trim().length > 0);
+        });
+        $(function() {
+            $('.uploadfield').focusout(function() {
+                var text_val = $(this).val();
+                $(this).parent().parent().parent().parent().toggleClass('not-empty', text_val !== "");
+            }).focusout();
+        });
+
+
+        $(".rd2").on("click", function() {
+            if ($(this).find('input[type="radio"]').is(':checked')) {
+                $('.radiolabel').removeClass('not-empty');
+                $('.radiolabel').addClass('not-empty');
+            }
+        });
+    });
 
   
 
