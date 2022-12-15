e5c8aba5cb85abeb26d45e99a9efbdf36103f974
Pankajchalke <pankaj.chalke@theminimalist.in>
2022-12-15 12:41:51 +0530

    case study listing page

--- wp-content/themes/theminimalist/case-study-listing.php
+++ wp-content/themes/theminimalist/case-study-listing.php
@@ -1,7 +1,7 @@
 <?php get_header(); /*Template Name: case studies listing three*/
 ?>  
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script> -->
     
 <!-- <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> -->
 
