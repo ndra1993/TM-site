46cec8e45d30899348a40a2be543d9479d66928a
Pankajchalke <pankaj.chalke@theminimalist.in>
2023-01-12 21:01:56 +0530

    updated case study pages

---
+++ wp-content/themes/theminimalist/tagged.php
@@ -0,0 +1,152 @@
+<?php get_header(); /*Template Name: tagged*/
+?>
+
+
+
+
+
+
+
+
+<a id="button"><img src="<?php bloginfo('template_directory'); ?>/images/back-to-top-arrow.svg"></a>
+
+<div class="taggedbgdetails">
+
+    <section>
+        <div class="sectionOne">
+            <div class="mobtext wow slideInLeft">
+                <p class="font20 fontW800 colorBlack">taggd</p>
+                <h1 class="font60 fontW800 colorBlack">Helping hiring managers & candidates</h1>
+                <h5 class="font20 colorWhite fontW500">By creating a digital platform that democratizes access to the art and science of hiring.</h5>
+            </div>
+            <div class="wow fadeInUp mobImg" data-wow-duration="4s">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-banner.png">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionTwo">
+            <div class="mobtextTwo wow slideInLeft" data-wow-duration="1s">
+                <div class="">
+                    <p class="font20 fontW800 colorBlack">insight</p>
+                    <h1 class="font60 fontW800 colorBlack">A new experience reflecting the brand’s vision and values</h1>
+                    <h5 class="font20 colorBlack fontW500">This digital recruitment platform helps candidates to get their dream job and hiring manager to get their right candidate. </h5>
+                </div>
+            </div>
+            <div class="mobimgTwo wow slideInRight" data-wow-duration="1s">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-banner-2.png">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionThree">
+            <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
+                <p class="font20 fontW800 colorBlack">solutions</p>
+                <h1 class="font60 fontW800 colorBlack">Introducing a unique, informative & clean  styling that create interest of users</h1>
+            </div>
+            <div class="mobsubtextThree wow slideInRight" data-wow-duration="1s">
+                <h5 class="font20 colorBlack fontW500">This website aims to educate the users about hiring the right way and eliminating an accidental hiring through easy navigation and easy access to the information</h5>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionFour">
+            <div class="desktopImg">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-1.png">
+            </div>
+            <div class="mobileImg">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-1.png">
+                
+            </div>
+        </div>
+    </section>
+
+    <section class="sectionSeven">
+        <div class="sectionSevenBox">
+            <div class="imgBox">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-3.png">
+            </div>
+            <div class="textBox">
+                <h3 class="colorBlack font30 fontW800">Matching candidates profiles</h3>
+                <p class="font24 fontW500">List of candidates with related qualities with detailed info and t-score</p>
+            </div>
+        </div>
+        <div class="sectionSevenBox">
+            <div class="textBox">
+                <h3 class="colorBlack font30 fontW800">Information popup with details</h3>
+                <p class="font24 fontW500">Can directly contact candidate and schedule a meeting for further</p>
+            </div>
+            <div class="imgBox">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-4.png">
+            </div>
+        </div>
+        <div class="sectionSevenBox">
+            <div class="imgBox">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-3.png">
+            </div>
+            <div class="textBox">
+                <h3 class="colorBlack font30 fontW800">Used unique illustration styling that helps to relate given information</h3>
+                <p class="font24 fontW500">The flat illustration styling used with some shadow and highlight effects to gave unique look and feel for illustration</p>
+            </div>
+        </div>
+        <div class="sectionSevenBox">
+            <div class="textBox">
+                <h3 class="colorBlack font30 fontW800">Tried to show detailed information upfront with actual data</h3>
+                <p class="font24 fontW500">The important information we tried to show upfront with the help of illustration. The user can easily navigate through toggle buttons if they are candidate or hiring managers.</p>
+            </div>
+            <div class="imgBox">
+                <img src="<?php bloginfo('template_directory'); ?>/images/tagged-4.png">
+            </div>
+        </div>
+    </section>
+
+    <section class="sectionEight">
+        <div class="sectionEightBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/tagged-5.png">
+        </div>
+        <div class="sectionEightBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/tagged-6.png">
+        </div>
+        <div class="sectionEightBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/tagged-7.png">
+        </div>
+        <div class="sectionEightBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/tagged-8.png">
+        </div>
+        <div class="sectionEightBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/tagged-9.png">
+        </div>
+        <div class="sectionEightBox">
+            <img src="<?php bloginfo('template_directory'); ?>/images/tagged-10.png">
+        </div>
+    </section>
+    <section>
+        <div class="sectionFive">
+            <div class="mobtextFour wow slideInLeft" data-wow-duration="1s">
+                <p class="font20 fontW800 colorBlack">impact</p>
+                <h1 class="font60 fontW800 colorBlack">A Brand that is Revolutionising recruitment landscape through democratising the art and science of hiring talent</h1>
+            </div>
+            <div class="mobsubtextFour wow slideInRight" data-wow-duration="1s">
+                <h5 class="font20 colorBlack fontW500">Combining the power of human knowledge and data, Taggd has successfully fulfilled talent mandates of more than 100+ Clients and ensured hiring managers’ success for half a million jobs from over 14+ sectors</h5>
+            </div>
+        </div>
+    </section>
+    <section class="sectionSix">
+        <div class="sectionSiximg"><img src="<?php bloginfo('template_directory'); ?>/images/tagged-2.png"></div>
+        <div class="moreCasestudy">
+            <div class="brandLogos">
+                <div class="relatedBox relatedBoxone" style="background: #ec2120;">
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/bizongo-logo.png"></a>
+                </div>
+                <div class="relatedBox relatedBoxtwo" style="background: #00C859;">
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/bizongo-logo.png"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+<?php get_footer(); ?>
+
