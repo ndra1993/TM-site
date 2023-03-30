49598b133616cf0bada965b4d482f8f16df5bd2e
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-03-30 22:29:05 +0530

    almarai case study

---
+++ wp-content/themes/theminimalist/almarai.php
@@ -0,0 +1,114 @@
+<?php get_header(); /*Template Name: almarai*/
+?>
+
+
+
+
+
+
+
+
+<a id="button"><img src="<?php bloginfo('template_directory'); ?>/images/back-to-top-arrow.svg"></a>
+
+<div class="dozeebgdetails almaraiBg">
+
+    <section>
+        <div class="sectionOne">
+            <div class="mobtext wow slideInLeft">
+                <p class="font20 fontW800 colorWhite">ALMARAI</p>
+                <h1 class="font60 fontW800 colorWhite">The largest vertically integrated dairy company in the Middle East</h1>
+                <h5 class="font20 colorWhite fontW500">Almarai operates in all 6 GCC countries. Having such a large base of customers, reducing their route-to-market cost was crucial to Almara</h5>
+            </div>
+            <div class="wow fadeInUp mobImg" data-wow-duration="4s">
+                <img src="<?php bloginfo('template_directory'); ?>/images/sec-one-img.png">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionTwo">
+            <div class="mobtextTwo wow slideInLeft" data-wow-duration="1s">
+                <div class="">
+                    <p class="font20 fontW800 colorBlack">insight</p>
+                    <h1 class="font60 fontW800 colorBlack">A new experience reflecting the brand’s vision and values</h1>
+                    <h5 class="font20 colorBlack fontW500">Almarai was still primitive when it came to handling their order management system. All communication, ordering and tracking happened only through the delivery agents for the customers. This increased the dependency on the delivery agents, lacked standardization and reduced transparency between Almarai and their customers</h5>
+                </div>
+            </div>
+            <div class="mobimgTwo wow slideInRight" data-wow-duration="1s">
+                <img src="<?php bloginfo('template_directory'); ?>/images/sec-two-img.png">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionThree">
+            <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
+                <p class="font20 fontW800 colorWhite">solutions</p>
+                <h1 class="font60 fontW800 colorWhite">Mobile app and website for customers to place and track orders</h1>
+            </div>
+            <div class="mobsubtextThree wow slideInRight" data-wow-duration="1s">
+                <h5 class="font20 colorWhite fontW500">A mobile-first approach was taken and later made responsive for desktops. The brand guidelines were followed and the interactions were kept simple for the less tech savvy audience</h5>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionFour">
+            <div class="desktopImg">
+                <img src="<?php bloginfo('template_directory'); ?>/images/sec-three-img.png">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionSeven">
+            <div class="sectionSevenBoxTwo">
+                <div class="sectionSevenBoxImg">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/sec-four-img.png">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/sec-four-img.png">
+                </div>
+                <div class="sectionSevenBoxText">
+                    <h3 class="fontW800 colorWhite font32">Bilingual Design</h3>
+                    <p class="fontW500 font16">The screens were designed in English & Arabic, providing a comprehensive and impactful user experience.</p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="sectionFour">
+            <div class="desktopImg">
+                <img src="<?php bloginfo('template_directory'); ?>/images/sec-five-img.png">
+            </div>
+            <div class="desktopImg">
+                <img src="<?php bloginfo('template_directory'); ?>/images/sec-six-img.png">
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <div class="sectionFive">
+            <div class="mobtextFour wow slideInLeft" data-wow-duration="1s">
+                <p class="font20 fontW800 colorBlack">impact</p>
+                <h1 class="font60 fontW800 colorBlack">Digital transformation for their ordering and tracking system </h1>
+            </div>
+            <div class="mobsubtextFour wow slideInRight" data-wow-duration="1s">
+                <h5 class="font20 colorBlack fontW500">This increased the dependency on the delivery agents, lacked standardization and reduced transparency between Almarai and their customers was solved by the digital transformation for their ordering and tracking system through an ecommerce platform. Usage of app also provide Almarai with easy access to data and increase sales by upselling through the platform</h5>
+            </div>
+        </div>
+    </section>
+    <section class="sectionSix">
+        <div class="sectionSiximg"><img src="<?php bloginfo('template_directory'); ?>/images/sec-seven-img.png"></div>
+        <div class="moreCasestudy">
+            <div class="brandLogos">
+                <div class="relatedBox relatedBoxone" style="background: #ec2120;">
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/bizongo-logo.png"></a>
+                </div>
+                <div class="relatedBox relatedBoxtwo" style="background: #00C859;">
+                    <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/bizongo-logo.png"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+
+<?php get_footer(); ?>
+
