bbc949e20c7d073c2a8e2743ae830b9fb17c2cf8
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-04-03 18:25:24 +0530

    font changes

---
+++ wp-content/themes/theminimalist/casestudy-details.php
@@ -0,0 +1,11 @@
+<?php get_header(); /*Template Name: case study details*/
+$homepage_id = get_option('page_on_front');
+
+?>
+<a id="button"><img src="<?php bloginfo('template_directory'); ?>/images/back-to-top-arrow.svg"></a>
+
+
+
+
+
+<?php get_footer(); ?>
