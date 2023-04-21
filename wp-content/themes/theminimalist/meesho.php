a2e4f3fbdd612982db0d9a4b4dfa16e53586949d
Pankajchalke <pankaj.chalke@theminimalist.in>
2023-04-21 13:11:41 +0530

    updated code

--- wp-content/themes/theminimalist/meesho.php
+++ wp-content/themes/theminimalist/meesho.php
@@ -5,21 +5,66 @@ $homepage_id = get_option('page_on_front');
 
 <a id="button"><img src="<?php bloginfo('template_directory'); ?>/images/back-to-top-arrow.svg"></a>
 
-<div class="casebgdetails">
-
-    <section>
+<div class="csdetailBg" style="background: linear-gradient(158.47deg, #070707 4.94%, #101010 53.55%, #070707 58.48%, #020202 65.02%, #131313 77.34%);">
+    <!-- Banner Section -->
+    <section class="bannerSection taggedBanner" style="background: linear-gradient(158.47deg, #070707 4.94%, #101010 53.55%, #070707 58.48%, #020202 65.02%, #131313 77.34%);">
+        <div class="sectionOne">
+            <div class="mobtext wow slideInLeft">
+                <p class="font20 fontW800 colorWhite">Meesho </p>
+                <h1 class="font60 fontW800 colorWhite">Beating the clock to launch Meesho's mobile-friendly website</h1>
+                <h5 class="font20 colorWhite fontW500">Meesho is the biggest and most reliable online marketplace in India for resellers that conduct their business over Facebook and WhatsApp. It is democratising e-commerce for all, thanks to its tech-first philosophy.</h5>
+            </div>
+            <div class="mobImg">
+                <div class="desktopImg">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/meesho-banner.svg">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <section>
         <div class="meeshobg sectionOne">
             <div class="mobtext wow slideInLeft">
                 <p class="font20 fontW800 colorWhite">Meesho </p>
                 <h1 class="font60 fontW800 colorWhite">Beating the clock to launch Meesho's mobile-friendly website</h1>
                 <h5 class="font20 colorWhite fontW500">Meesho is the biggest and most reliable online marketplace in India for resellers that conduct their business over Facebook and WhatsApp. It is democratising e-commerce for all, thanks to its tech-first philosophy.</h5>
             </div>
-            <div class="wow bounceInUp mobImg" data-wow-duration="4s">
+            <div class="wow fadeInDown mobImg" data-wow-duration="4s">
                 <img src="<?php bloginfo('template_directory'); ?>/images/meesho-banner.svg">
             </div>
         </div>
+    </section> -->
+    <!-- lefttext-rightimg -->
+    <section class="lefttext-rightImg" style="background: #fff;">
+        <div class="sectionTwo">
+            <div class="mobtextTwo wow slideInLeft" data-wow-duration="1s" style="background: #fff;">
+                <div class="innerContent">
+                    <p class="font20 fontW800 colorBlack">Objective </p>
+                    <h1 class="font60 fontW800 colorBlack">Making Meesho accessible via Mobile </h1>
+                    <h5 class="font20 colorBlack fontW500">Meesho intended to create a mobile version of their existing website, to improve UX and increase visibility for SEO under a time constraint</h5>
+                </div>
+            </div>
+            <div class="mobimgTwo wow slideInRight" data-wow-duration="1s">
+                <img src="<?php bloginfo('template_directory'); ?>/images/meesho1.png">
+            </div>
+        </div>
     </section>
-    <section>
+    <!-- leftheading-rightText -->
+    <section class="leftheading-rightText" style="background: linear-gradient(158.47deg, #070707 4.94%, #101010 53.55%, #070707 58.48%, #020202 65.02%, #131313 77.34%);">
+        <div class="sectionThree">
+            <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
+                <div class="innerContianer">
+                    <p class="font20 fontW800 colorWhite">Solutions </p>
+                    <h1 class="font60 fontW800 colorWhite">Optimizing for success</h1>
+                </div>
+            </div>
+            <div class="mobsubtextThree wow slideInRight" data-wow-duration="1s">
+                <div class="innerContianertwo">
+                    <h5 class="font20 colorWhite fontW500">To achieve true performance optimization, all 3D elements were recomposed, and animations were tailored to fit the mobile screen for ease of navigation, minimal load time, and an easy call to action.</h5>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <section>
         <div class="sectionTwo">
             <div class="mobtextTwo wow slideInLeft" data-wow-duration="1s">
                 <div class="">
@@ -32,8 +77,8 @@ $homepage_id = get_option('page_on_front');
                 <img src="<?php bloginfo('template_directory'); ?>/images/meesho1.png">
             </div>
         </div>
-    </section>
-    <section>
+    </section> -->
+    <!-- <section>
         <div class="meeshobg sectionThree">
             <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
                 <p class="font20 fontW800 colorWhite">Solutions </p>
@@ -44,12 +89,26 @@ $homepage_id = get_option('page_on_front');
             </div>
         </div>
         
-    </section>
+    </section> -->
     <section>
         <div id="meesho-beforeafter"></div>
     </section>
-    
-    <section>
+    <!-- leftimg-righttext -->
+    <section class="lefttext-rightImg leftimg-rightText">
+        <div class="sectionTwo">
+            <div class="mobtextTwo wow slideInRight" data-wow-duration="1s" style="background: none;">
+                <div class="innerContent">
+                    <p class="font20 fontW800 colorBlack">Impact  </p>
+                    <h1 class="font60 fontW800 colorBlack">The delightful user experience delivered before time  </h1>
+                    <h5 class="font20 colorBlack fontW500">All assets were successfully delivered ahead of schedule, giving the client the luxury of time to get internal approvals and move assets into development for their mobile website launch.</h5>
+                </div>
+            </div>
+            <div class="mobimgTwo wow slideInLeft" data-wow-duration="1s">
+                <img src="<?php bloginfo('template_directory'); ?>/images/meesho2.png">
+            </div>
+        </div>
+    </section>
+    <!-- <section>
         <div class="sectionTwo">
             <div class="mobimgTwo wow slideInLeft" data-wow-duration="1s">
                 <img src="<?php bloginfo('template_directory'); ?>/images/meesho2.png">
@@ -62,8 +121,24 @@ $homepage_id = get_option('page_on_front');
                 </div>
             </div>
         </div>
+    </section> -->
+    <!-- leftheading-rightText -->
+    <section class="leftheading-rightText" style="background: linear-gradient(158.47deg, #070707 4.94%, #101010 53.55%, #070707 58.48%, #020202 65.02%, #131313 77.34%);">
+        <div class="sectionThree">
+            <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
+                <div class="innerContianer">
+                    <p class="font20 fontW800 colorWhite">Solutions </p>
+                    <h1 class="font60 fontW800 colorWhite">A clean  design with an emphasis on the information</h1>
+                </div>
+            </div>
+            <div class="mobsubtextThree wow slideInRight" data-wow-duration="1s">
+                <div class="innerContianertwo">
+                    <h5 class="font20 colorWhite fontW500">A webiste that educates the users about how Dozee uses AI and machine learning to transform hospitals all over India.</h5>
+                </div>
+            </div>
+        </div>
     </section>
-    <section>
+    <!-- <section>
         <div class="meeshobg sectionFive">
             <div class="mobtextFour wow slideInLeft" data-wow-duration="1s">
                 <p class="font20 fontW800 colorWhite">Solutions </p>
@@ -73,15 +148,33 @@ $homepage_id = get_option('page_on_front');
                 <h5 class="font20 colorWhite fontW500">A webiste that educates the users about how Dozee uses AI and machine learning to transform hospitals all over India.</h5>
             </div>
         </div>
-    </section>
-    <section>
-        <div class="sectionFourMeesho">
+    </section> -->
+
+    <!-- fullscreenbigimg -->
+    <section class="fullscreenBigimg">
+        <div class="sectionFour">
             <div class="desktopImg">
                 <img src="<?php bloginfo('template_directory'); ?>/images/meesho3.png">
             </div>
         </div>
     </section>
-    <section>
+    <!-- leftheading-rightText -->
+    <section class="leftheading-rightText" style="background: #fff;">
+        <div class="sectionThree">
+            <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
+                <div class="innerContianer">
+                    <p class="font20 fontW800 colorBlack">Impact </p>
+                <h1 class="font60 fontW800 colorBlack">A memorable and unique identity that makes the brand stand out</h1>
+                </div>
+            </div>
+            <div class="mobsubtextThree wow slideInRight" data-wow-duration="1s">
+                <div class="innerContianertwo">
+                    <h5 class="font20 colorBlack fontW500">The user demographic  on this platform are from Tier 2 and Tier 3 cities who do not know much about WEB3 & it’s implications. They wanted an Informative website to educate their users and help them embark on the journey together with Oneto11.</h5>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <section>
         <div class="sectionFive">
             <div class="mobtextFour wow slideInLeft" data-wow-duration="1s">
                 <p class="font20 fontW800 colorBlack">Impact </p>
@@ -91,12 +184,36 @@ $homepage_id = get_option('page_on_front');
                 <h5 class="font20 colorBlack fontW500">The user demographic  on this platform are from Tier 2 and Tier 3 cities who do not know much about WEB3 & it’s implications. They wanted an Informative website to educate their users and help them embark on the journey together with Oneto11.</h5>
             </div>
         </div>
-    </section>
+    </section> -->
+    
     <section class="sectionFourMeesho">
         <div id="animation-container" class="desktopMeeshoSlider"></div>
         <div id="meesho-mobileslider" class="mobileMeeshoSlider"></div>
     </section>
     <section>
+        <div class="sectionFourMeesho">
+            <div class="desktopImg">
+                <img src="<?php bloginfo('template_directory'); ?>/images/meesho-footer.png">
+            </div>
+        </div>
+    </section>
+    <!-- leftheading-rightText -->
+    <section class="leftheading-rightText" style="background: #fff;">
+        <div class="sectionThree">
+            <div class="mobtextThree wow slideInLeft" data-wow-duration="1s">
+                <div class="innerContianer">
+                    <p class="font20 fontW800 colorBlack">Impact </p>
+                    <h1 class="font60 fontW800 colorBlack">Delightful user experience delivered before time</h1>
+                </div>
+            </div>
+            <div class="mobsubtextThree wow slideInRight" data-wow-duration="1s">
+                <div class="innerContianertwo">
+                    <h5 class="font20 colorBlack fontW500">All assets were successfully delivered ahead of schedule, giving the client the luxury of time to get internal approvals and move assets into development for their mobile website launch.</h5>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <section>
         <div class="sectionFive">
             <div class="mobtextFour wow slideInLeft" data-wow-duration="1s">
                 <p class="font20 fontW800 colorBlack">Impact </p>
@@ -106,16 +223,10 @@ $homepage_id = get_option('page_on_front');
                 <h5 class="font20 colorBlack fontW500">All assets were successfully delivered ahead of schedule, giving the client the luxury of time to get internal approvals and move assets into development for their mobile website launch.</h5>
             </div>
         </div>
-    </section>
-    <section>
-        <div class="sectionFourMeesho">
-            <div class="desktopImg">
-                <img src="<?php bloginfo('template_directory'); ?>/images/meesho4.png">
-            </div>
-        </div>
-    </section>
+    </section> -->
+    
     <section class="sectionSix bottomimgLogos">
-        <div class="sectionSiximg"><img src="<?php bloginfo('template_directory'); ?>/images/meesho-footer.png"></div>
+        <div class="sectionSiximg"><img src="<?php bloginfo('template_directory'); ?>/images/meesho4.png"></div>
         <div class="moreCasestudy">
             <div class="brandLogos">
                 <div class="relatedBox relatedBoxone" style="background: #ec2120;">
