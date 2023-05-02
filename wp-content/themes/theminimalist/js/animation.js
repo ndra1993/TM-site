bbffcd50a55c8a44376532c4226189c6edefa8ec
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-05-02 10:17:44 +0530

    animation js

---
+++ wp-content/themes/theminimalist/js/animation.js
@@ -0,0 +1,9 @@
+// lotttie animation tagged js starts
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animationmblmoving'),
+    path: site_url+'/wp-content/themes/theminimalist/js/json/mblAnime.json', // Required
+    renderer: 'svg', // or 'canvas', 'html'
+    loop: true,
+    autoplay: true
+  });
+  // lotttie animation tagged js ends
