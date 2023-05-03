5025224cc803d3a8df12bbdf7558acf1dc49a70e
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-05-03 18:22:52 +0530

    COMPONENTS

--- wp-content/themes/theminimalist/components/bottom_image_with_relatedpost.php
+++ wp-content/themes/theminimalist/components/bottom_image_with_relatedpost.php
@@ -1,14 +1,37 @@
-<!-- Bottom Image With Relatedpost -->
-<section class="bottomimgLogos">
-  <div class="sectionSiximg"><img src="<?php bloginfo('template_directory'); ?>/images/phone-four.png"></div>
+<!-- bottom logo section-->
+<section class="bottomimgLogos" style="background: #fff">
+  <div class="sectionSiximg">
+    <?php $bottomfooterimage = get_sub_field('bottom_footer_image');
+    if (!empty($bottomfooterimage)) : ?>
+      <img src="<?php echo esc_url($bottomfooterimage['url']); ?>" loading="lazy" alt="<?php echo esc_attr($bottomfooterimage['alt']); ?>" />
+    <?php endif; ?>
+  </div>
   <div class="moreCasestudy">
     <div class="brandLogos">
-      <div class="relatedBox relatedBoxone" style="background: #ec2120;">
-        <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/nayaka.png"></a>
-      </div>
-      <div class="relatedBox relatedBoxtwo" style="background: #00C859;">
-        <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/bizongo-logo.png"></a>
-      </div>
+      <?php if (have_rows('related_post_one')) : ?>
+        <?php while (have_rows('related_post_one')) : the_row(); ?>
+          <div class="relatedBox relatedBoxone" style="background: <?php echo get_sub_field('background_color'); ?>;">
+            <a href="#">
+              <?php $relatedpostimage = get_sub_field('related_post_image');
+              if (!empty($relatedpostimage)) : ?>
+                <img src="<?php echo esc_url($relatedpostimage['url']); ?>" loading="lazy" alt="<?php echo esc_attr($relatedpostimage['alt']); ?>" />
+              <?php endif; ?>
+            </a>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+      <?php if (have_rows('related_post_two')) : ?>
+        <?php while (have_rows('related_post_two')) : the_row(); ?>
+          <div class="relatedBox relatedBoxtwo" style="background: <?php echo get_sub_field('background_color'); ?>;">
+            <a href="#">
+              <?php $relatedpostimage = get_sub_field('related_post_image');
+              if (!empty($relatedpostimage)) : ?>
+                <img src="<?php echo esc_url($relatedpostimage['url']); ?>" loading="lazy" alt="<?php echo esc_attr($relatedpostimage['alt']); ?>" />
+              <?php endif; ?>
+            </a>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
     </div>
   </div>
 </section>
