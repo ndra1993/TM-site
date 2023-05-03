81e8aba46d35e735039f11d270be0416654d3db4
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-05-03 22:58:11 +0530

    components

--- wp-content/themes/theminimalist/components/left_image_right_content.php
+++ wp-content/themes/theminimalist/components/left_image_right_content.php
@@ -8,10 +8,20 @@
         <p class="font20 fontW500"><?php echo get_sub_field('description'); ?></p>
       </div>
     </div>
-    <div class="mobimgTwo wow slideInLeft" data-wow-duration="1s" style="background: <?php echo get_sub_field('image_box_background_color'); ?>;">
-      <?php $leftimage = get_sub_field('left_image');
-      if( !empty( $leftimage ) ): ?>
-        <img src="<?php echo esc_url($leftimage['url']); ?>" alt="<?php echo esc_attr($leftimage['alt']); ?>" />
+    
+    <div class="mobimgTwo wow slideInRight" data-wow-duration="1s" style="background: <?php echo get_sub_field('image_box_background_color'); ?>;">
+      <?php if (have_rows('left_image_options')) : ?>
+        <?php while (have_rows('left_image_options')) : the_row(); ?>
+          <?php if (get_row_layout() == 'left_image_box') : ?>
+            <?php $leftimage = get_sub_field('left_image');
+            if (!empty($leftimage)) : ?>
+              <img src="<?php echo esc_url($leftimage['url']); ?>" loading="lazy" alt="<?php echo esc_attr($leftimage['alt']); ?>" />
+            <?php endif; ?>
+
+          <?php elseif (get_row_layout() == 'left_animation_box') : ?>
+            <?php echo get_sub_field('left_animation'); ?>
+          <?php endif; ?>
+        <?php endwhile; ?>
       <?php endif; ?>
     </div>
   </div>
