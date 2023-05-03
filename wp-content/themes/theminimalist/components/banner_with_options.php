e9822d66ab1431b1b34731a2938a3fd25b083c84
Bhavika191 <bhavika.parmar@theminimalist.in>
2023-05-03 17:01:22 +0530

    flexible content

--- wp-content/themes/theminimalist/components/banner_with_options.php
+++ wp-content/themes/theminimalist/components/banner_with_options.php
@@ -4,24 +4,27 @@
     <div class="mobtext wow slideInLeft" style="color: <?php echo get_sub_field('text_color'); ?>;">
       <h5 class="font20 fontW800"><?php echo get_sub_field('sub_heading'); ?></h5>
       <h1 class="font60 fontW800"><?php echo get_sub_field('heading'); ?></h1>
-      <p class="font20 fontW500"><?php echo get_sub_field('description'); ?></p>
+      <p class="font20 fontW500"><?php echo get_sub_field('description'); ?></>
     </div>
-    <div class="mobImg">
-      <?php if( have_rows('banner_option') ): ?>
-      <?php while( have_rows('banner_option') ): the_row();?>
-          <?php if( get_row_layout() == 'banner_image_box' ): ?>
+
+    <?php if (have_rows('banner_option')) : ?>
+      <?php while (have_rows('banner_option')) : the_row(); ?>
+
+        <?php if (get_row_layout() == 'banner_image_box') : ?>
+          <div class="mobImg">
             <?php $bannerimage = get_sub_field('banner_image');
-            if( !empty( $bannerimage ) ): ?>
+            if (!empty($bannerimage)) : ?>
               <img src="<?php echo esc_url($bannerimage['url']); ?>" loading="lazy" alt="<?php echo esc_attr($bannerimage['alt']); ?>" />
             <?php endif; ?>
+          </div>
 
-          <?php elseif( get_row_layout() == 'banner_animation_box' ): ?>
+        <?php elseif (get_row_layout() == 'banner_animation_box') : ?>
+          <div class="bannerAnimeimg">
             <?php echo get_sub_field('banner_animation'); ?>
-
-          <?php endif;?>
-        <?php endwhile; ?>
-      <?php endif; ?>
-
-    </div>
+          </div>
+          
+        <?php endif; ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
   </div>
 </section>
--- wp-content/themes/theminimalist/components/left_content_right_image.php
+++ wp-content/themes/theminimalist/components/left_content_right_image.php
@@ -8,10 +8,20 @@
         <p class="font20 fontW500"><?php echo get_sub_field('description'); ?></p>
       </div>
     </div>
+
     <div class="mobimgTwo wow slideInRight" data-wow-duration="1s" style="background: <?php echo get_sub_field('image_box_background_color'); ?>;">
-      <?php $rightimage = get_sub_field('right_image');
-      if( !empty( $rightimage ) ): ?>
-        <img src="<?php echo esc_url($rightimage['url']); ?>" alt="<?php echo esc_attr($rightimage['alt']); ?>" />
+      <?php if (have_rows('right_image_options')) : ?>
+        <?php while (have_rows('right_image_options')) : the_row(); ?>
+          <?php if (get_row_layout() == 'right_image_box') : ?>
+            <?php $rightimage = get_sub_field('right_image');
+            if (!empty($rightimage)) : ?>
+              <img src="<?php echo esc_url($rightimage['url']); ?>" loading="lazy" alt="<?php echo esc_attr($rightimage['alt']); ?>" />
+            <?php endif; ?>
+
+          <?php elseif (get_row_layout() == 'right_animation_box') : ?>
+            <?php echo get_sub_field('right_animation'); ?>
+          <?php endif; ?>
+        <?php endwhile; ?>
       <?php endif; ?>
     </div>
   </div>
