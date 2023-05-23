9f94f6ba82e61a65816a819cf961e1362d7d7377
Pankajchalke <pankaj.chalke@theminimalist.in>
2023-05-23 10:11:19 +0530

    updated code

--- wp-content/themes/theminimalist/header.php
+++ wp-content/themes/theminimalist/header.php
@@ -51,8 +51,11 @@
 
 	<header>
 		<div class="headerContent">
-			
-
+		
+			              
+                           
+		<!-- <?php echo  get_page_template() ?> -->
+		
 			<?php if (is_front_page()) { ?>
 				<!-- Homepage Header -->
 				<div class="logoBox">
@@ -72,11 +75,27 @@
 					<a class="backArrow" onclick="GoBackWithRefresh();return false;"><img src="<?php bloginfo('template_directory'); ?>/images/arrow-title.svg" alt="Back Arrow"></a>
 					<h3 class="fontW800 font30 fontPoppins colorBlack" style="text-transform:capitalize;"><?php the_title(); ?></h3>
 				</div>
-			<?php } else{ ?>
+			<?php } elseif (is_page ('casestudy-details')) { ?>
+				<div class="logoBox titleBox">
+					<a class="backArrow" onclick="GoBackWithRefresh();return false;"><img src="<?php bloginfo('template_directory'); ?>/images/arrow-title.svg" alt="Back Arrow"></a>
+					<h3 class="fontW800 font30 fontPoppins colorBlack" style="text-transform:capitalize;"><?php echo get_the_title( $post->ID ); ?> </h3>
+				</div>
+			<?php } else{ ?> 
 			<div class="logoBox titleBox">
 				<a class="backArrow" onclick="GoBackWithRefresh();return false;"><img src="<?php bloginfo('template_directory'); ?>/images/arrow-title.svg" alt="Back Arrow"></a>
 				<h3 class="fontW800 font30 fontPoppins colorBlack" style="text-transform:capitalize;">
-					
+					<?php
+                            $terms = get_the_category( $post->ID, 'categories' );
+                            $links = array();
+                            foreach ( $terms as $term ) {
+                                $links[] = $term->slug;
+                            }
+                            
+                            
+                             $tax_links = @str_replace('-', ' ', $links[0]);
+//                             echo $links[0];
+                           echo $tax_links;
+                        ?>
 				</h3>
 			</div>
 
